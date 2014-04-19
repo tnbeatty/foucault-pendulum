3d5bd81fb1936cba9c788ffff58cf7132b0aaf5e
Nate Beatty <nate.beatty@gmail.com>
2014-04-18 22:30:02 -0400

    Skybox created

--- js/main.js
+++ js/main.js
@@ -19,35 +19,72 @@ var MYENV = { // The variables that define the game environment
     lightColor: 0xF7EFBE
   },
   pendulum: {
-    length: 350
+    length: 350,
+    rotRateEarth: 360, // degrees per day
+    latitude: 0 // latitude of the pendulum on the earth
   }
 }
 
 // Global variables
-var scene, camera, renderer;
+var scene, skyscene, camera, skycamera, renderer;
 var pBob, pPivot;
 var clock = new THREE.Clock(true);
 var isAnimating = true;
 
 function init() {
   scene = new THREE.Scene();
+  skyscene = new THREE.Scene();
 
   // Set up the camera
-  camera = new THREE.PerspectiveCamera(60, MYENV.aspect, 1, 1000); // FOV, Aspect, Near, Far
-  camera.position.y = 100;
-  camera.position.z = 500;
+  camera = new THREE.PerspectiveCamera(60, MYENV.aspect, 1, 100000); // FOV, Aspect, Near, Far
+  camera.position.y = 200;
+  camera.position.z = 700;
   scene.add(camera);
+
+  skycamera = new THREE.PerspectiveCamera(60, MYENV.aspect, 1, 100000);
 
   // Set up the scene
   buildScene();
+
+  var path = "assets/textures/";
+  var format = '.jpg';
+  var urls = [
+    path + 'px' + format, path + 'nx' + format,
+    path + 'py' + format, path + 'ny' + format,
+    path + 'pz' + format, path + 'nz' + format
+  ];
+
+  var textureCube = THREE.ImageUtils.loadTextureCube(urls, new THREE.CubeRefractionMapping());
+  var material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    envMap: textureCube,
+    refractionRatio: 0.95
+  });
+
+  // Skybox
+
+  var shader = THREE.ShaderLib["cube"];
+  shader.uniforms["tCube"].value = textureCube;
+
+  var material = new THREE.ShaderMaterial({
+
+    fragmentShader: shader.fragmentShader,
+    vertexShader: shader.vertexShader,
+    uniforms: shader.uniforms,
+    depthWrite: false,
+    side: THREE.BackSide
+
+  }), mesh = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 100), material);
+  skyscene.add(mesh);
 
   // Render the scene
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(MYENV.width, MYENV.height);
 
   // Add canvas to the DOM
-  renderer.domElement.style.backgroundColor = MYENV.scene.bgColor;
-  renderer.setClearColor(MYENV.scene.bgColor);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.autoClear = false;
   document.getElementById('container').appendChild(renderer.domElement);
 
 }
@@ -83,7 +120,6 @@ function buildScene() {
   );
   scene.add(pBob);
 
-
   // Add the lighting
   var directionalLight1 = new THREE.DirectionalLight(MYENV.lightColor, 0.7);
   directionalLight1.position.set(0.5, 1, 0.5);
@@ -102,12 +138,18 @@ function render() {
 
   // Swing the pendulum
   var timeElapsed = clock.getElapsedTime();
-  var T = Math.sqrt( MYENV.pendulum.length / (980) );
+  var timeDelta = clock.getDelta();
+  var T = Math.sqrt(MYENV.pendulum.length / (980));
   var theta = THREE.Math.degToRad(10) * Math.cos(timeElapsed / T);
   pBob.position.x = MYENV.pendulum.length * Math.sin(theta);
   pBob.position.y = MYENV.pendulum.length - (MYENV.pendulum.length * Math.cos(theta));
-  // pBob.position.y = MYENV.pendulum.length;
 
+  var rotRate = MYENV.pendulum.rotRateEarth * Math.cos(MYENV.pendulum.latitude);
+
+  camera.lookAt(scene.position);
+  skycamera.rotation.copy(camera.rotation);
+
+  renderer.render(skyscene, skycamera);
   renderer.render(scene, camera);
 }
 
